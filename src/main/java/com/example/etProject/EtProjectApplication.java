6913bb1aad01881217810ab19b75a2490f76b2a8
dagyeomellow <jungerika0109@gmail.com>
2024-04-05 16:28:35 +0900

    최초파일생성

---
+++ src/main/java/com/example/etProject/EtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.etProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/etProject/EtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
