1800e57e0357a72c482ba6a55d24a2b690ed518c
TaehwanAn <conscious52th@gmail.com>
2024-04-11 16:11:57 +0900

    저장요

---
+++ src/main/java/com/example/etProject/dto/ProducersDTO.java
@@ -0,0 +1,26 @@
+package com.example.etProject.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ProducersDTO {
+
+    private String producerId;
+    private String memberId;
+    private Double locationX;
+    private Double locationY;
+    private Double installedCapacity;
+
+    
+}
